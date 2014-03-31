42cb592fd1983081a45132ab9dba197b5872dac2
Jaroslaw Waliszko <jaroslaw.waliszko@gmail.com>
2014-03-31 21:00:18 +0200

    AssemblyInfo supplemented. Redundant references removed.

--- src/ExpressiveAnnotations.MvcUnobtrusiveValidatorProvider/Properties/AssemblyInfo.cs
+++ src/ExpressiveAnnotations.MvcUnobtrusiveValidatorProvider/Properties/AssemblyInfo.cs
@@ -1,18 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ExpressiveAnnotations.MvcUnobtrusiveValidatorProvider")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ABB")]
-[assembly: AssemblyProduct("ExpressiveAnnotations.MvcUnobtrusiveValidatorProvider")]
-[assembly: AssemblyCopyright("Copyright © ABB 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © Jaroslaw Waliszko 2014")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
