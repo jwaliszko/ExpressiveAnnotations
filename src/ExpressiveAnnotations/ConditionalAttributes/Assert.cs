eb3c55d932225e327e1f40773f4eb49cff17610f
Jaroslaw Waliszko <jaroslaw.waliszko@gmail.com>
2014-05-06 21:58:16 +0200

    Types consistency assertion fix for nullable types.

--- src/ExpressiveAnnotations/ConditionalAttributes/Assert.cs
+++ src/ExpressiveAnnotations/ConditionalAttributes/Assert.cs
@@ -10,7 +10,16 @@ namespace ExpressiveAnnotations.ConditionalAttributes
             if (targetValue == null || string.Equals(targetValue as string, "*"))
                 return; // type doesn't matter when null or star is involved
 
-            if (dependentField.PropertyType != targetValue.GetType())
+            var propertyType = dependentField.PropertyType;
+            propertyType = propertyType.IsGenericType && propertyType.GetGenericTypeDefinition() == typeof (Nullable<>)
+                ? Nullable.GetUnderlyingType(propertyType)
+                : propertyType;
+            var targetType = targetValue.GetType();
+            targetType = targetType.IsGenericType && targetType.GetGenericTypeDefinition() == typeof (Nullable<>)
+                ? Nullable.GetUnderlyingType(targetType)
+                : targetType;
+
+            if (propertyType != targetType)
                 throw new InvalidOperationException(
                     string.Format(
                         "Type mismatch detected in {0} definition for {1} field. Types consistency is required for {2} field and its corresponding target value (unless target is null or *).",
