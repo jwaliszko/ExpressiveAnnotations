2e8508e179553dbd0e108bc2d55f8f231c28b6e8
Jaroslaw Waliszko <jaroslaw.waliszko@gmail.com>
2014-06-09 22:25:05 +0200

    Unit tests for new analysis.

---
+++ src/ExpressiveAnnotations.Tests/LexerTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using ExpressiveAnnotations.LogicalExpressionsAnalysis.LexicalAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExpressiveAnnotations.Tests
+{
+    [TestClass]
+    public class LexerTest
+    {
+        [TestMethod]
+        public void Verify_lexer_logic()
+        {
+            const string expression =
+                "GoAbroad == true " +
+                    "&& (" +
+                            "(NextCountry != \"european country\" && NextCountry == Country.Name) " +
+                            "|| (Age > 24 && Age <= 55.5)" +
+                        ")";
+
+            var lexer = new Lexer();
+            var tokens = lexer.Analyze(expression);
+            Assert.AreEqual(tokens.Length, 25);
+            Assert.AreEqual(tokens[0].Value, "GoAbroad");
+            Assert.AreEqual(tokens[1].Value, "==");
+            Assert.AreEqual(tokens[2].Value, true);
+            Assert.AreEqual(tokens[3].Value, "&&");
+            Assert.AreEqual(tokens[4].Value, "(");
+            Assert.AreEqual(tokens[5].Value, "(");
+            Assert.AreEqual(tokens[6].Value, "NextCountry");
+            Assert.AreEqual(tokens[7].Value, "!=");
+            Assert.AreEqual(tokens[8].Value, "european country");
+            Assert.AreEqual(tokens[9].Value, "&&");
+            Assert.AreEqual(tokens[10].Value, "NextCountry");
+            Assert.AreEqual(tokens[11].Value, "==");
+            Assert.AreEqual(tokens[12].Value, "Country.Name");
+            Assert.AreEqual(tokens[13].Value, ")");
+            Assert.AreEqual(tokens[14].Value, "||");
+            Assert.AreEqual(tokens[15].Value, "(");
+            Assert.AreEqual(tokens[16].Value, "Age");
+            Assert.AreEqual(tokens[17].Value, ">");
+            Assert.AreEqual(tokens[18].Value, 24);
+            Assert.AreEqual(tokens[19].Value, "&&");
+            Assert.AreEqual(tokens[20].Value, "Age");
+            Assert.AreEqual(tokens[21].Value, "<=");
+            Assert.AreEqual(tokens[22].Value, 55.5f);
+            Assert.AreEqual(tokens[23].Value, ")");
+            Assert.AreEqual(tokens[24].Value, ")");
+
+            try
+            {
+                lexer.Analyze("true + false");
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e is ArgumentException);
+                Assert.IsTrue(e.Message == "Lexer error. Unexpected token started at + false.");
+            }
+
+            try
+            {
+                lexer.Analyze("true && ^");
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e is ArgumentException);
+                Assert.IsTrue(e.Message == "Lexer error. Unexpected token started at ^.");
+            }
+        }
+    }
+}
---
+++ src/ExpressiveAnnotations.Tests/ParserTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using ExpressiveAnnotations.LogicalExpressionsAnalysis.SyntacticAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExpressiveAnnotations.Tests
+{
+    [TestClass]
+    public class ParserTest
+    {
+        private class Model
+        {
+            public Model SubModel { get; set; }
+
+            public DateTime Date { get; set; }
+            public int Number { get; set; }
+            public bool Flag { get; set; }
+            public string Text { get; set; }
+        }
+
+        [TestMethod]
+        public void Verify()
+        {
+            var parser = new Parser();
+            Assert.IsTrue(parser.Parse<object>("true").Invoke(null));
+            Assert.IsFalse(parser.Parse<object>("false").Invoke(null));
+            Assert.IsFalse(parser.Parse<object>("!true").Invoke(null));
+            Assert.IsTrue(parser.Parse<object>("!false").Invoke(null));
+
+            Assert.IsTrue(parser.Parse<object>("true && true").Invoke(null));
+            Assert.IsFalse(parser.Parse<object>("false && false").Invoke(null));
+            Assert.IsFalse(parser.Parse<object>("true && false").Invoke(null));
+            Assert.IsFalse(parser.Parse<object>("false && true").Invoke(null));
+
+            Assert.IsTrue(parser.Parse<object>("true || true").Invoke(null));
+            Assert.IsFalse(parser.Parse<object>("false || false").Invoke(null));
+            Assert.IsTrue(parser.Parse<object>("true || false").Invoke(null));
+            Assert.IsTrue(parser.Parse<object>("false || true").Invoke(null));
+
+            Assert.IsTrue(parser.Parse<object>("0 == 0").Invoke(null));
+            Assert.IsFalse(parser.Parse<object>("0 != 0").Invoke(null));
+            Assert.IsTrue(parser.Parse<object>("0 >= 0").Invoke(null));
+            Assert.IsTrue(parser.Parse<object>("0 <= 0").Invoke(null));
+
+            Assert.IsFalse(parser.Parse<object>("0 == 1").Invoke(null));
+            Assert.IsTrue(parser.Parse<object>("0 != 1").Invoke(null));
+            Assert.IsFalse(parser.Parse<object>("0 >= 1").Invoke(null));
+            Assert.IsFalse(parser.Parse<object>("0 > 1").Invoke(null));
+            Assert.IsTrue(parser.Parse<object>("0 <= 1").Invoke(null));
+            Assert.IsTrue(parser.Parse<object>("0 < 1").Invoke(null));
+        }
+
+        [TestMethod]
+        public void VerifyContext()
+        {
+            var model = new Model
+            {
+                Date = DateTime.Now,
+                Number = 0,
+                Flag = true,
+                Text = "hello parser",
+                SubModel = new Model
+                {
+                    Date = DateTime.Now.AddDays(1),
+                    Number = 1,
+                    Flag = false,
+                    Text = "hello parser"
+                }
+            };
+
+            var parser = new Parser();
+            Assert.IsTrue(parser.Parse(model.GetType(), "Flag").Invoke(model));
+            Assert.IsFalse(parser.Parse(model.GetType(), "!Flag").Invoke(model));
+            Assert.IsTrue(parser.Parse(model.GetType(), "Flag && true").Invoke(model));
+
+            Assert.IsFalse(parser.Parse(model.GetType(), "SubModel.Flag").Invoke(model));
+            Assert.IsTrue(parser.Parse(model.GetType(), "!SubModel.Flag").Invoke(model));
+            Assert.IsFalse(parser.Parse(model.GetType(), "SubModel.Flag && true").Invoke(model));
+
+            Assert.IsTrue(parser.Parse(model.GetType(), "Number < SubModel.Number").Invoke(model));
+
+            const string expression =
+                "Flag == true " +
+                    "&& (" +
+                            "(Text != \"hello parser\" && Date < SubModel.Date) " +
+                            "|| (Number >= 0  && Number < 1)" +
+                        ")";
+
+            Assert.IsTrue(parser.Parse(model.GetType(), expression).Invoke(model));
+        }
+    }
+}
