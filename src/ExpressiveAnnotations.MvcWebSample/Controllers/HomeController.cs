4598804e7561161caf0ec9d03cc99610acfb580c
Jaroslaw Waliszko <jaroslaw.waliszko@gmail.com>
2014-09-18 23:13:09 +0200

    PRG to avoid subsequent form submission attempts on page refresh.

--- src/ExpressiveAnnotations.MvcWebSample/Controllers/HomeController.cs
+++ src/ExpressiveAnnotations.MvcWebSample/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace ExpressiveAnnotations.MvcWebSample.Controllers
     public class HomeController : BaseController
     {
         public ActionResult Index()
-        {
+        {            
             var model = new Query
             {
                 GoAbroad = true,
@@ -18,6 +18,9 @@ namespace ExpressiveAnnotations.MvcWebSample.Controllers
                 LatestSuggestedReturnDate = DateTime.Today.AddMonths(1),
                 ContactDetails = new Contact()
             };
+
+            ViewBag.Success = TempData["Success"];
+
             return View("Home", model);
         }
 
@@ -25,7 +28,11 @@ namespace ExpressiveAnnotations.MvcWebSample.Controllers
         public ActionResult Index(Query model)
         {
             if (ModelState.IsValid)
-                ViewBag.Success = "Success";            
+            {
+                TempData["Success"] = "Query successfully submitted";
+                return RedirectToAction("Index"); // PRG to avoid subsequent form submission attempts on page refresh (http://en.wikipedia.org/wiki/Post/Redirect/Get)
+            }
+
             return View("Home", model);
         }
     }
