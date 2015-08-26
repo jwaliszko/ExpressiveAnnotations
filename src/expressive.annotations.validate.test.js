afc74f1d6fc7752971b8bda43dc08c1700e45923
Jaroslaw Waliszko <jaroslaw.waliszko@gmail.com>
2015-08-26 18:06:34 +0200

    Unit test for complete foem validation added.

--- src/expressive.annotations.validate.test.js
+++ src/expressive.annotations.validate.test.js
@@ -628,4 +628,26 @@
         qunit.ok(!result);
     });
 
+    qunit.test("verify_sample_form_validation", function() {
+
+        ea.addMethod('Whoami', function() {
+            return 'root';
+        });
+        ea.addMethod('ArrayContains', function(value, array) {
+            return $.inArray(value, array) >= 0;
+        });
+        ea.addValueParser('ArrayParser', function(value, field) {
+            qunit.equal(field, 'ContactDetails.Letters');
+
+            var array = value.split(',');
+            return array.length === 0 ? null : array;
+        });
+
+        var validator = $('#basic_test_form').validate();
+        var element = $('#basic_test_form').find('[name="ContactDetails.Email"]');
+        var result = element.valid(); // trigger wait for result (all is synchronous)
+        qunit.ok(!result);
+        qunit.equal(validator.numberOfInvalids(), 1);
+    });
+
 }($, QUnit, window.ea, window.ea.___6BE7863DC1DB4AFAA61BB53FF97FE169));
