e5c2a5a2d3f29eecbd6c4753264bf3e6d568fdde
Jaroslaw Waliszko <jaroslaw.waliszko@gmail.com>
2015-08-29 23:05:08 +0200

    Radio buttons binding fixed in WPF sample.

---
+++ src/ExpressiveAnnotations.MvvmDesktopSample/Misc/RadioButtonCheckedConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ExpressiveAnnotations.MvvmDesktopSample.Misc
+{
+    public class RadioButtonCheckedConverter : IValueConverter //http://stackoverflow.com/q/397556/270315
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.Equals(parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.Equals(true) ? parameter : Binding.DoNothing;
+        }
+    }
+}
---
+++ src/ExpressiveAnnotations.MvvmDesktopSample/Misc/StringToBooleanConverter.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ExpressiveAnnotations.MvvmDesktopSample.Misc
+{
+    public class StringToBooleanConverter : IValueConverter //http://stackoverflow.com/q/18449890/270315
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool) value == bool.Parse((string) parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return bool.Parse((string) parameter);
+        }
+    }
+}
