6c9bbf502b63d4502075bd37b5142bc81b7109fc
Jaroslaw Waliszko <jaroslaw.waliszko@gmail.com>
2015-11-30 19:06:21 +0100

    Powershell tests-invoking script fixes (to be executed on AppVeyor without friction).

--- build/test.ps1
+++ build/test.ps1
@@ -1,30 +1,33 @@
-$XUnitDir = get-childitem ..\src\ xunit.console.exe -recurse | select-object -first 1 | select -expand Directory
-$XUnitCmd = "& '$($XUnitDir)\xunit.console.exe' ..\src\ExpressiveAnnotations.Tests\bin\Debug\ExpressiveAnnotations.Tests.dll ..\src\ExpressiveAnnotations.MvcUnobtrusive.Tests\bin\Debug\ExpressiveAnnotations.MvcUnobtrusive.Tests.dll ..\src\ExpressiveAnnotations.MvcWebSample.UITests\bin\Debug\ExpressiveAnnotations.MvcWebSample.UITests.dll -xml .\xunit-results.xml -appveyor"
-Invoke-Expression $XUnitCmd
-
-$ChutzpahDir = get-childitem ..\src\ chutzpah.console.exe -recurse | select-object -first 1 | select -expand Directory
-$ChutzpahCmd = "& '$($ChutzpahDir)\chutzpah.console.exe' /path ..\src\expressive.annotations.validate.test.js /path ..\src\tests.html /junit .\chutzpah-results.xml"
-Invoke-Expression $ChutzpahCmd
-
-$results = [xml](get-content .\chutzpah-results.xml)
-
-$anyFailures = $FALSE
-foreach ($testsuite in $results.testsuites.testsuite) {
-    foreach ($testcase in $testsuite.testcase) {
-        if ($testcase.failure) {            
-			if (Get-Command Add-AppveyorTest -errorAction SilentlyContinue) {
-				Add-AppveyorTest $testcase.name -Outcome Failed -FileName $testsuite.name -ErrorMessage $testcase.failure.message -Duration $testcase.time
-			}
-            $anyFailures = $TRUE
-        }
-        else {
-			if (Get-Command Add-AppveyorTest -errorAction SilentlyContinue) {
-				Add-AppveyorTest $testcase.name -Outcome Passed -FileName $testsuite.name -Duration $testcase.time
-			}
-        }
-    }
+$RootDir = "..\"
+if($env:APPVEYOR -eq $True) {
+    $RootDir = $env:APPVEYOR_BUILD_FOLDER
 }
 
-if ($anyFailures -eq $TRUE) {
-    #$host.SetShouldExit(1)
+$XUnitDir = get-childitem $RootDir xunit.console.exe -recurse | select-object -first 1 | select -expand Directory
+$XUnitCmd = "$XUnitDir\xunit.console.exe $RootDir\src\ExpressiveAnnotations.Tests\bin\Debug\ExpressiveAnnotations.Tests.dll $RootDir\src\ExpressiveAnnotations.MvcUnobtrusive.Tests\bin\Debug\ExpressiveAnnotations.MvcUnobtrusive.Tests.dll $RootDir\src\ExpressiveAnnotations.MvcWebSample.UITests\bin\Debug\ExpressiveAnnotations.MvcWebSample.UITests.dll -xml .\xunit-results.xml -appveyor"
+Invoke-Expression $XUnitCmd
+
+$ChutzpahDir = get-childitem $RootDir chutzpah.console.exe -recurse | select-object -first 1 | select -expand Directory
+$ChutzpahCmd = "$ChutzpahDir\chutzpah.console.exe /path $RootDir\src\expressive.annotations.validate.test.js /path $RootDir\src\tests.html /junit .\chutzpah-results.xml"
+Invoke-Expression $ChutzpahCmd
+
+if($env:APPVEYOR -eq $True) {
+    $success = $True
+    $results = [xml](get-content .\chutzpah-results.xml)
+
+    foreach ($testsuite in $results.testsuites.testsuite) {
+        foreach ($testcase in $testsuite.testcase) {
+            if ($testcase.failure) {
+                Add-AppveyorTest $testcase.name -Outcome Failed -FileName $testsuite.name -ErrorMessage $testcase.failure.message -Duration $testcase.time
+                $success = $False
+            }
+            else {
+                Add-AppveyorTest $testcase.name -Outcome Passed -FileName $testsuite.name -Duration $testcase.time
+            }
+        }
+    }
+
+    if ($success -eq $False) {
+        $host.SetShouldExit(1)
+    }
 }
