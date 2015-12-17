bbac4ea72c3663874e2a33a6c17cad0011fcb877
jwaliszko <jaroslaw.waliszko@gmail.com>
2015-12-17 16:47:16 +0100

    Unstable test fixed (load missing assemblies from specific directory).

--- src/ExpressiveAnnotations.MvcWebSample.UITests/AttributesCompilationTest.cs
+++ src/ExpressiveAnnotations.MvcWebSample.UITests/AttributesCompilationTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using ExpressiveAnnotations.Attributes;
 using Xunit;
 
@@ -29,6 +30,18 @@ namespace ExpressiveAnnotations.MvcWebSample.UITests
             }
             return attributes;
         }
+
+        public static string GetAssemblyLocationFromWebSampleFolder(string assemblyName)
+        {
+            return Path.GetFullPath(Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\ExpressiveAnnotations.MvcWebSample\bin\", assemblyName));
+        }
+
+        public static Assembly LoadFromWebSampleFolder(object sender, ResolveEventArgs args)
+        {
+            var assemblyPath = GetAssemblyLocationFromWebSampleFolder($"{new AssemblyName(args.Name).Name}.dll");
+            return !File.Exists(assemblyPath) ? null : Assembly.LoadFrom(assemblyPath);
+        }
     }
 
     public class AttributesCompilationTest
@@ -36,11 +49,35 @@ namespace ExpressiveAnnotations.MvcWebSample.UITests
         [Fact]
         public void annotations_used_in_application_are_compiled_with_success() // reveals compile-time errors (no need to wait for application startup)
         {
-            var applicationAssembly = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
-                @"..\..\..\ExpressiveAnnotations.MvcWebSample\bin\ExpressiveAnnotations.MvcWebSample.dll"));
-            var assembly = Assembly.LoadFrom(applicationAssembly);
-            var attribs = assembly.CompileExpressiveAttributes();
-            Assert.Equal(27, attribs.Count());
+            try
+            {
+                AppDomain.CurrentDomain.AssemblyResolve += Helper.LoadFromWebSampleFolder;
+                var assemblyPath = Helper.GetAssemblyLocationFromWebSampleFolder("ExpressiveAnnotations.MvcWebSample.dll");
+                var assembly = Assembly.LoadFrom(assemblyPath);
+                var attribs = assembly.CompileExpressiveAttributes();
+                Assert.Equal(27, attribs.Count());
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                var sb = new StringBuilder();
+                sb.AppendLine(e.Message).AppendLine();
+                sb.AppendLine("LoaderExceptions:").AppendLine();
+                foreach (var loaderEx in e.LoaderExceptions)
+                {
+                    sb.AppendLine(loaderEx.Message).AppendLine();
+                    var fileNotFoundEx = loaderEx as FileNotFoundException;
+                    if (string.IsNullOrEmpty(fileNotFoundEx?.FusionLog))
+                        continue;
+
+                    sb.AppendLine("FusionLog:");
+                    sb.AppendLine(fileNotFoundEx.FusionLog);
+                }
+                throw new Exception(sb.ToString());
+            }
+            finally
+            {
+                AppDomain.CurrentDomain.AssemblyResolve -= Helper.LoadFromWebSampleFolder;
+            }
         }
     }
 }
