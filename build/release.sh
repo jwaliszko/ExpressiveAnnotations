ccf014bb31a1b552bef5cc728248e8c1b6f9996f
jwaliszko <jaroslaw.waliszko@gmail.com>
2016-01-07 04:34:41 +0100

    Assemblies signing operation moved to release script (all configurations can be build in VS without friction).

--- build/release.sh
+++ build/release.sh
@@ -1,64 +1,75 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-    echo "product version required"
+if [ $# -ne 2 ]; then
+    echo "usage: release.sh product-version keyfile-path"
     exit 1
 fi
 
-echo "------ cleanup..."
-rm -fr $1
-rm -fr ../src/bin
+echo -e "\n------ vacuum..."
+rm -fr $1 net40 net45
 
 if [ ! -f nuget.exe ]; then
-    echo "------ nuget.exe downloading..."
+    echo "\n------ nuget.exe downloading..."
     wget -nv 'http://dist.nuget.org/win-x86-commandline/latest/nuget.exe'
 fi
 
-echo "------ package restoration..."
+echo -e "\n------ package restoration..."
 ./nuget.exe restore ../src/ExpressiveAnnotations.sln
 
 msbuild="/cygdrive/c/Program Files (x86)/Msbuild/14.0/bin/MSBuild.exe"
 
-echo "------ release config compilation..."
-"$msbuild" ../src/ExpressiveAnnotations.sln /t:Build /p:Configuration=Release /verbosity:minimal
+echo -e "\n------ release-net40 config compilation..."
+"$msbuild" /nologo ../src/ExpressiveAnnotations.MvcUnobtrusive/ExpressiveAnnotations.MvcUnobtrusive.csproj "/t:Clean;Rebuild" /p:BuildProjectReferences=true /p:Configuration=Release /p:AssemblyOriginatorKeyFile=$2 /p:TreatWarningsAsErrors=true /p:TargetFrameworkVersion=v4.0 "/p:DefineConstants=NET40 SIGNED" /p:SignAssembly=true /verbosity:minimal
 
-echo "------ release-net40 config compilation..."
-"$msbuild" ../src/ExpressiveAnnotations.sln /t:Build /p:Configuration=Release-Net40 /verbosity:minimal
-echo "------ release-net45 config compilation..."
-"$msbuild" ../src/ExpressiveAnnotations.sln /t:Build /p:Configuration=Release-Net45 /verbosity:minimal
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 
-echo "------ generating documentation..."
-"$msbuild" ../doc/api/api.shfbproj /t:Build /p:Configuration=Release
+mkdir -p net40
+cp ../src/ExpressiveAnnotations.MvcUnobtrusive/bin/Release/ExpressiveAnnotations* net40
 
-echo "------ cutting off debug section from script..."
+echo -e "\n------ release-net45 config compilation..."
+"$msbuild" /nologo ../src/ExpressiveAnnotations.MvcUnobtrusive/ExpressiveAnnotations.MvcUnobtrusive.csproj "/t:Clean;Rebuild" /p:BuildProjectReferences=true /p:Configuration=Release /p:AssemblyOriginatorKeyFile=$2 /p:TreatWarningsAsErrors=true /p:TargetFrameworkVersion=v4.5 "/p:DefineConstants=NET45 SIGNED" /p:SignAssembly=true /verbosity:minimal
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+mkdir -p net45
+cp ../src/ExpressiveAnnotations.MvcUnobtrusive/bin/Release/ExpressiveAnnotations* net45
+
+echo -e "\n------ help file generation..."
+"$msbuild" /nologo ../doc/api/api.shfbproj /t:Build /p:Configuration=Release /verbosity:minimal
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+echo -e "\n------ script debug section removal..."
 sed '/!debug section enter/,/!debug section leave/{N;d;}' ../src/expressive.annotations.validate.js > expressive.annotations.validate.js
 
-echo "------ generating minified script version..."
+echo -e "\n------ script minification..."
 uglifyjs --compress --mangle --comments /Copyright/ --output expressive.annotations.validate.min.js -- expressive.annotations.validate.js
 cp expressive.annotations.validate.min.js ../src/expressive.annotations.validate.min.js
 
-echo "------ core dll version extraction..."
-core_ver=`strings ../src/bin/Release-Net40/ExpressiveAnnotations.dll | egrep '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' | cut -f1,2,3 -d"."`
+echo -e "\n------ core version extraction..."
+core_ver=`strings ../src/ExpressiveAnnotations/bin/Release/ExpressiveAnnotations.dll | egrep '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' | cut -f1,2,3 -d"."`
 
-echo "------ packages building..."
+echo -e "\n------ package building..."
 ./nuget.exe pack ExpressiveAnnotations.nuspec -version $1 -symbols
 ./nuget.exe pack ExpressiveAnnotations.dll.nuspec -version $core_ver -symbols
 
-echo "------ archive creation..."
+echo -e "\n------ archive creation..."
 mkdir -p $1/ExpressiveAnnotations{/net40,/net45}
-mv ../src/bin/Release-Net40/ExpressiveAnnotations* $1/ExpressiveAnnotations/net40
-mv ../src/bin/Release-Net45/ExpressiveAnnotations* $1/ExpressiveAnnotations/net45
+mv net40 $1/ExpressiveAnnotations
+mv net45 $1/ExpressiveAnnotations
 mv expressive.annotations.validate* $1/ExpressiveAnnotations
 mv *.nupkg $1
 cp ../doc/api/api.chm $1/ExpressiveAnnotations
 cd $1 && zip -r ExpressiveAnnotations.zip ExpressiveAnnotations
-
-echo "------ cleanup..."
 cd ..
-"$msbuild" ../src/ExpressiveAnnotations.sln /t:Clean /p:Configuration=Release /verbosity:quiet
-"$msbuild" ../src/ExpressiveAnnotations.sln /t:Clean /p:Configuration=Release-Net40 /verbosity:quiet
-"$msbuild" ../src/ExpressiveAnnotations.sln /t:Clean /p:Configuration=Release-Net45 /verbosity:quiet
+
+echo -e "\nsuccess"
 
 # nuget setapikey ...
 # nuget push ExpressiveAnnotations.x.x.x.nupkg
-# nuget push ExpressiveAnnotations.dll.x.x.x.nupkg
