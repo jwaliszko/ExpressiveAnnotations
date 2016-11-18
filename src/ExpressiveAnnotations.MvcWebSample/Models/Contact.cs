c1193519027a4aa68864c8bdfe2ee94341c74a12
jwaliszko <jaroslaw.waliszko@gmail.com>
2016-11-18 03:14:54 +0100

    Phone validation regex added. C# version, taken from PhoneAttribute implementation (using lookbehind feature), adjusted to work in JavaScript as well.

--- src/ExpressiveAnnotations.MvcWebSample/Models/Contact.cs
+++ src/ExpressiveAnnotations.MvcWebSample/Models/Contact.cs
@@ -20,11 +20,12 @@ namespace ExpressiveAnnotations.MvcWebSample.Models
 
         [RequiredIf("Email == null",
             ErrorMessageResourceType = typeof (Resources), ErrorMessageResourceName = nameof(Resources.EmailOrPhoneRequired))]
+        //[AssertThat("IsPhone(Phone)")]
         [AssertThat(@"IsRegexMatch(Phone, '^\\d+$')", // regex pattern escaped despite verbatim string - it's because our expressive language parser
                                                       // verbatim syntax should be perfectly valid with that one JavaScript accepts 
             ErrorMessageResourceType = typeof (Resources), ErrorMessageResourceName = nameof(Resources.DigitsOnlyAccepted), Priority = 1)]
         [AssertThat("Length(Phone) > 8 && Length(Phone) < 16",
-            ErrorMessageResourceType = typeof (Resources), ErrorMessageResourceName = nameof(Resources.RangeViolated), Priority = 2)]
+            ErrorMessageResourceType = typeof (Resources), ErrorMessageResourceName = nameof(Resources.RangeViolated), Priority = 2)]        
         [Display(ResourceType = typeof (Resources), Name = nameof(Resources.Phone))]
         public string Phone { get; set; }
 
--- src/expressive.annotations.validate.js
+++ src/expressive.annotations.validate.js
@@ -204,6 +204,10 @@ var
                 // taken from HTML5 specification: http://www.w3.org/TR/html5/forms.html#e-mail-state-(type=email)
                 return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(str);
             });
+            this.addMethod('IsPhone', function(str) {
+                // taken from PhoneAttribute implementation: https://referencesource.microsoft.com/#System.ComponentModel.DataAnnotations/DataAnnotations/PhoneAttribute.cs, adjusted for JavaScript regex engine - since JS doesn't use lookbehind feature, alternative approach used (as described here: http://stackoverflow.com/q/7376238/270315)
+                return /^(\+\s?)?((?!\+.*)\(\+?\d+([\s\-\.]?\d+)?\)|\d+)([\s\-\.]?(\(\d+([\s\-\.]?\d+)?\)|\d+))*(\s?(x|ext\.?)\s?\d+)?$/.test(str);
+            });
             this.addMethod('IsUrl', function(str) {
                 // contributed by Diego Perini: https://gist.github.com/dperini/729294 (https://mathiasbynens.be/demo/url-regex)
                 return /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/i.test(str);
